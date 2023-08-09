34d138c57f03da74a79ee6a3612c0526bef541b8
James Hally <32179923+jnha@users.noreply.github.com>
2023-08-09 14:41:59 -0700

    French language orthography with tests

--- language/french.py
+++ language/french.py
@@ -1,6 +1,17 @@
 """French Language"""
 
+character_map = {
+    "ch": "\u0283",
+    "tt": "t",
+    "r": "\u0280",
+    "e": "\u025b",
+}
+
 
 def orthography_to_phonology(text: str) -> str:
     """Parse a written french into a phonologic representation"""
+    if text[-1:] in ("t", "e"):
+        text = text[:-1]
+    for letter, sound in character_map.items():
+        text = text.replace(letter, sound)
     return text
--- tests/test_french.py
+++ tests/test_french.py
@@ -3,12 +3,15 @@ import pytest
 
 from language.french import orthography_to_phonology
 
+
 @pytest.mark.parametrize(
     "orthography,phonology",
     [
-        ('chat', '\u0283a')
-    ]
+        ("", ""),
+        ("chat", "\u0283a"),
+        ("chatte", "\u0283at"),
+        ("lettre", "l\u025bt\u0280"),
+    ],
 )
 def test_orthography_word(orthography, phonology):
     assert orthography_to_phonology(orthography) == phonology
-
