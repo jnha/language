06267b61977a35f317d6f8436cdb570ab409f7b6
James Hally <32179923+jnha@users.noreply.github.com>
2023-08-09 20:46:56 -0700

    Handle french r, ou, and soft g

--- language/spanish.py
+++ language/spanish.py
@@ -1,42 +1,40 @@
-
-
 def orthography_to_phonology(text: str) -> str:
     """Convert from written spanish to a phonemic ipa transcription"""
     i = 0
     out = []
     text = text.lower()
     while i < len(text):
-        elif text[i] == 'r':
-            if text[i+1] == 'r':
-                out.append('r')
+        if text[i] == "r":
+            if text[i + 1] == "r":
+                out.append("r")
             else:
-                out.append('ɾ')
-        elif text[i] == 'l':
-            if text[i+1] == 'l':
-                out.append('ʎ')
+                out.append("ɾ")
+        elif text[i] == "l":
+            if text[i + 1] == "l":
+                out.append("ʎ")
             else:
-                out.append('l')
-        elif text[i] == 'c':
-            if text[i+1] in 'ei':
-                out.append('θ')
-            elif text[i+1] == 'h':
-                out.append('t͡ʃ')
+                out.append("l")
+        elif text[i] == "c":
+            if text[i + 1] in "ei":
+                out.append("θ")
+            elif text[i + 1] == "h":
+                out.append("t͡ʃ")
             else:
-                out.append('k')
-        elif text[i] == 'h':
+                out.append("k")
+        elif text[i] == "h":
             pass
-        elif text[i] == 'q':
-            out.append('q')
-        elif text[i] == 'y':
-            if text[i-1] not in 'eaoiu' or text[i+1] in 'eaiou':
-                out.append('ʝ')
-        elif text[i] == '.':
-            out.append(' ‖')
-        elif text[i] == ',':
-            out.append(' |')
+        elif text[i] == "q":
+            out.append("q")
+        elif text[i] == "y":
+            if text[i - 1] not in "eaoiu" or text[i + 1] in "eaiou":
+                out.append("ʝ")
+        elif text[i] == ".":
+            out.append(" ‖")
+        elif text[i] == ",":
+            out.append(" |")
         elif not text[i].isalpha():
             out.append(text[i])
         else:  # Default no change to handle numbers and foreign scripts
             out.append(text[i])
         i += 1
-    return ''.join(out)
+    return "".join(out)
--- tests/test_french.py
+++ tests/test_french.py
@@ -10,7 +10,9 @@ from language.french import orthography_to_phonology
         ("", ""),
         ("chat", "\u0283a"),
         ("chatte", "\u0283at"),
+        ("fait", "f\u025b"),
         ("lettre", "l\u025bt\u0280"),
+        ("rouge", "\u0280u\u0292"),
     ],
 )
 def test_orthography_word(orthography, phonology):
