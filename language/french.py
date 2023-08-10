8b1165466330c01440a2858b8826dacb504891ee
James Hally <32179923+jnha@users.noreply.github.com>
2023-08-10 13:58:26 -0700

    More french test cases

--- language/french.py
+++ language/french.py
@@ -3,17 +3,25 @@
 character_map = {
     "ch": "\u0283",
     "tt": "t",
+    "ll": "l",
+    "hu": "\u0265",
+    "eau": "o",
     "ai": "\u025b",
     "ou": "u",
+    "om": "\u0254\u0303",
+    "an": "\u0251\u0303",
     "r": "\u0280",
     "e": "\u025b",
     "g": "\u0292",
+    "é": "e",
+    "ç": "s",
 }
 
 
 def orthography_to_phonology(text: str) -> str:
     """Parse a written french into a phonologic representation"""
-    if text[-1:] in ("t", "e"):
+    text = text.lower()
+    if text[-1:] in "tesb":
         text = text[:-1]
     for letter, sound in character_map.items():
         text = text.replace(letter, sound)
